e94f4eab8a59a2e2f415232a31720db135264dba
xorphitus <xorphitus@gmail.com>
2016-01-25 22:30:53 +0900

    AOT バージョンのフィボナッチ

--- simple-aot/src/simple_aot/core.clj
+++ simple-aot/src/simple_aot/core.clj
@@ -1,5 +1,11 @@
 (ns simple-aot.core
   (:gen-class))
 
+(defn fib
+  ([n]
+   (take n (fib 1 1)))
+  ([a b]
+   (cons a (lazy-seq (fib b (+ a b))))))
+
 (defn -main []
-  (println "Hello, World!"))
+  (println (fib 20)))
