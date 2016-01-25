690571d4938f09d0a462e4074f3e916e081a0929
xorphitus <xorphitus@gmail.com>
2016-01-25 22:20:21 +0900

    AOT なしで普通のフィボナッチ

--- simple-non-aot/src/simple_non_aot/core.clj
+++ simple-non-aot/src/simple_non_aot/core.clj
@@ -1,6 +1,10 @@
 (ns simple-non-aot.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn fib
+  ([n]
+   (take n (fib 1 1)))
+  ([a b]
+   (cons a (lazy-seq (fib b (+ a b))))))
+
+(defn -main []
+  (println (fib 20)))
