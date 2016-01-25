595cca209c66f76d9716b50a6ef2b0f7fcb92bd7
xorphitus <xorphitus@gmail.com>
2016-01-25 22:09:50 +0900

    lein new simple-non-aot

---
+++ simple-non-aot/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to simple-non-aot
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ simple-non-aot/test/simple_non_aot/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-non-aot.core-test
+  (:require [clojure.test :refer :all]
+            [simple-non-aot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
